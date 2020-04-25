be36abac32ca85e04fd63002abb7979518972c4d
Subela <olindasuely@yahoo.com.br>
2020-04-25 20:55:06 +0000

    configure Character and Details

--- mobdev/src/app/pages/character-details/character-details.page.ts
+++ mobdev/src/app/pages/character-details/character-details.page.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FavouriteService } from '../../services/favourite.service';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 @Component({
-  selector: 'app-character-details',
-  templateUrl: './character-details.page.html',
-  styleUrls: ['./character-details.page.scss'],
+    selector: 'app-character-details',
+    templateUrl: './character-details.page.html',
+    styleUrls: ['./character-details.page.scss'],
 })
 export class CharacterDetailsPage implements OnInit {
 
-  constructor() { }
+    character: any;
+    isFavourite = false;
+    characterId = null;
 
-  ngOnInit() {
+    constructor(private activatedRoute: ActivatedRoute, private api: ApiService, private favouriteService: FavouriteService) { }
+
+    ngOnInit() {
+
+        this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
+
+        this.api.getCharacter(this.characterId).subscribe(res => {
+            this.character = res[0];
+        });
+
+        this.favouriteService.isFavouriteC(this.characterId).then(isFav => {
+            this.isFavourite = isFav;
+        });
+    }
+
+  favouriteEpisode() {
+    this.favouriteService.favouriteCharacter(this.characterId).then(() => {
+      this.isFavourite = true;
+    });
   }
+ 
+  unfavouriteEpisode() {
+    this.favouriteService.unfavouriteCharacter(this.characterId).then(() => {
+      this.isFavourite = false;
+    });
+    }
 
 }
--- mobdev/src/app/services/favourite.service.ts
+++ mobdev/src/app/services/favourite.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
 const STORAGE_KEY = 'favouriteEpisodes';
+const STORAGE_KEY1 = 'favouriteCharacters';
 
 @Injectable({
   providedIn: 'root'
@@ -37,6 +38,36 @@ export class FavouriteService {
                 var index = result.indexOf(episodeId);
                 result.splice(index, 1);
                 return this.storage.set(STORAGE_KEY, result);
+            }
+        });
+    }
+    getAllFavouriteCharacters() {
+        return this.storage.get(STORAGE_KEY1);
+    }
+
+    isFavouriteC(characterId) {
+        return this.getAllFavouriteCharacters().then(result => {
+            return result && result.indexOf(characterId) !== -1;
+        });
+    }
+
+    favouriteCharacter(characterId) {
+        return this.getAllFavouriteCharacters().then(result => {
+            if (result) {
+                result.push(characterId);
+                return this.storage.set(STORAGE_KEY1, result);
+            } else {
+                return this.storage.set(STORAGE_KEY1, [characterId]);
+            }
+        });
+    }
+
+    unfavouriteCharacter(characterId) {
+        return this.getAllFavouriteCharacters().then(result => {
+            if (result) {
+                var index = result.indexOf(characterId);
+                result.splice(index, 1);
+                return this.storage.set(STORAGE_KEY1, result);
             }
         });
     }
